28383be5ea10a8891058c9f417b145c73dd59d93
Moiz Ahmed <moizah96@gmail.com>
2023-11-15 18:45:08 -0500

    Processed JSON and passed values to another function to display data on the frontend

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import "./style.css";
 
+const city = document.querySelector(".city");
+const temperature = document.querySelector(".temperature");
+const feelsLike = document.querySelector(".feels-like");
+const humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".wind");
+
+let weatherData = [];
+
 async function getWeather() {
   const response = await fetch(
-    "https://api.weatherapi.com/v1/current.json?key=066ec9f33ab64d4db68224838231411&q=toronto",
-    { mode: "cors" }
+    "https://api.weatherapi.com/v1/current.json?key=066ec9f33ab64d4db68224838231411&q=toronto"
   );
-  const weatherData = await response.json();
-  console.log(weatherData);
+  let data = await response.json();
+  return data;
 }
 
-getWeather();
+async function displayWeather() {
+  let weatherData = await getWeather();
+  console.log(weatherData.location);
+  console.log(weatherData.current);
+  city.textContent += ` ${weatherData.location.name}`;
+  temperature.textContent += ` ${weatherData.current.temp_c}`;
+  feelsLike.textContent += ` ${weatherData.current.feelslike_c}`
+  humidity.textContent += ` ${weatherData.current.humidity}`
+  wind.textContent += ` ${weatherData.current.wind_kph}`
+
+}
+
+displayWeather();
