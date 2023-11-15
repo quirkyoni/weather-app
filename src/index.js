88bf125b35d44282603e814d4f5022530df1355c
Moiz Ahmed <moizah96@gmail.com>
2023-11-14 19:26:16 -0500

    Wrote function that hit the API and returned weather data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./style.css";
+
+async function getWeather() {
+  const response = await fetch(
+    "https://api.weatherapi.com/v1/current.json?key=066ec9f33ab64d4db68224838231411&q=toronto",
+    { mode: "cors" }
+  );
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
+
+getWeather();
