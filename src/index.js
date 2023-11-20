d66cab4d5fba092a190c2d6f0da5333f0519d36b
Moiz Ahmed <moizah96@gmail.com>
2023-11-20 16:16:40 -0500

    Added click event listener to the Submit button to display the search value's weather data

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ const temperature = document.querySelector(".temperature");
 const humidity = document.querySelector(".humidity");
 const wind = document.querySelector(".wind");
 
-async function getWeather() {
+async function getWeather(searchValue) {
   const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=066ec9f33ab64d4db68224838231411&q=toronto`,
+    `https://api.weatherapi.com/v1/current.json?key=066ec9f33ab64d4db68224838231411&q=${searchValue}`,
     { mode: "cors" }
   );
   const weatherData = await response.json();
@@ -22,44 +22,28 @@ async function getWeather() {
 }
 
 const handleWeatherData = (weatherData) => {
-  console.log(weatherData.location)
+  console.log(weatherData.location);
+  countryCity.textContent = "";
+  countryCity.textContent = `${weatherData.location.country}, ${weatherData.location.name}`;
+  temperature.textContent = "";
+  temperature.textContent = `${weatherData.current.temp_c} ℃`;
+  humidity.textContent = "";
+  humidity.textContent = `${weatherData.current.humidity}`;
+  wind.textContent = "";
+  wind.textContent = `${weatherData.current.wind_kph} km/h`;
 };
 
-async function main() {
+async function main(searchValue) {
   try {
-    const weatherData = await getWeather();
+    const weatherData = await getWeather(searchValue);
     handleWeatherData(weatherData);
   } catch (error) {
     console.log(error.error);
   }
 }
 
-main()
-// async function searchFunc() {
-//   submit.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     let searchValue = search.value;
-//     const response = fetch(
-//       `https://api.weatherapi.com/v1/current.json?key=066ec9f33ab64d4db68224838231411&q=${searchValue}`,
-//       { mode: "cors" }
-//     )
-//       .then(function (response) {
-//         console.log(response);
-//         let data = response.json();
-//         return data;
-//       })
-//       .then(function (data) {
-//         console.log(data)
-//         countryCity.textContent = "";
-//         countryCity.textContent = `${data.location.country}, ${data.location.name}`;
-//         temperature.textContent = "";
-//         temperature.textContent = `${data.current.temp_c} ℃`;
-//         humidity.textContent = "";
-//         humidity.textContent = `${data.current.humidity}`;
-//         wind.textContent = "";
-//         wind.textContent = `${data.current.wind_kph} km/h`;
-//       });
-//   });
-// }
-
-// searchFunc();
+submit.addEventListener("click", (e) => {
+  e.preventDefault()
+  let searchValue = search.value
+  main(searchValue)
+})
