d43be72f61280d621e37f7658911ef471ddbcf62
Moiz Ahmed <moizah96@gmail.com>
2024-01-21 20:33:36 -0500

    Debugging images

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,28 @@
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index-custom.html", // Specify a different filename
+      template: "dist/index.html", // Path to your HTML template
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
