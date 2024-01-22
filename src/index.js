53937b86e0e38e915f6c4c525ddb9e75bb256472
Moiz Ahmed <moizah96@gmail.com>
2024-01-21 20:12:40 -0500

    Debugging images issue

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import "./style.css";
 
-import temperatureImg from "./temperature.png";
-import windImg from "./wind.png";
-import humidityImg from "./humidity.png";
-
 const search = document.querySelector(".search");
 const submit = document.querySelector(".submit");
 
